38205db44f99687602bc049e1458c6c343bbfad6
eciara <ciara.ewelina@gmail.com>
2024-08-21 13:26:18 +0200

    Added content to the 'Our Team' section, including team members and their roles.

---
+++ src/components/OurTeam/OurTeamList.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="col-sm-6 col-md-3 our-team-box" v-for="item in team" :key="item.id">
+        <div class="our-team-box__figure">
+            <img :src="item.img" :alt="`${item.name} - ${item.position}`" class="our-team-box__image img-fluid">
+        </div>
+        <div class="our-team-box__copy">
+            <h4 class="our-team-box__title">{{ item.name }}</h4>
+            <p class="our-team-box__subtitle">{{ item.position }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'OurTeamList',
+    data() {
+        return {
+            team: [
+                {
+                    id: 1,
+                    img: './../../images/our-team/our-team_1.jpg',
+                    name: 'Amelia Olson',
+                    position: 'Ceramicist'
+                },
+                {
+                    id: 2,
+                    img: './../../images/our-team/our-team_2.jpg',
+                    name: 'George Evans',
+                    position: 'Ceramics technologist'
+                },
+                {
+                    id: 3,
+                    img: './../../images/our-team/our-team_3.jpg',
+                    name: 'Olivia Conley',
+                    position: 'Ceramics designer'
+                },
+                {
+                    id: 4,
+                    img: './../../images/our-team/our-team_4.jpg',
+                    name: 'Thomas Hanson',
+                    position: 'Ceramic glazier'
+                },
+            ],
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.our-team-box {
+    &__image {
+        border-radius: .75rem;
+    }
+    &__copy {
+        text-align: center;
+        padding: .5rem .5rem 0;
+    }
+    &__subtitle {
+        font-size: .85rem;
+    }
+}
+</style>
---
+++ src/components/OurTeam/OurTeamSection.vue
@@ -0,0 +1,46 @@
+<template>
+    <section class="section our-team">
+        <div class="container">
+            <header class="our-team__header">
+                <h2 class="our-team__title">People behind Pottero</h2>
+            </header>
+            <div class="our-team__list">
+                <div class="row g-4">
+                    <OurTeamList />
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import OurTeamList from './OurTeamList.vue';
+export default {
+    name: 'OurTeamSection',
+    components: {
+        OurTeamList
+    }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/_variables.scss';
+
+.our-team {
+    &__header {
+        text-align: center;
+        margin-bottom: 2.75rem;
+
+        &:after {
+            content: '';
+            position: relative;
+            top: 1rem;
+            display: block;
+            margin-inline: auto;
+            background-color: $accent-color;
+            width: 5rem;
+            height: 3px;
+        }
+    }
+}
+</style>
