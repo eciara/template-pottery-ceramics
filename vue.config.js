da43c7184fe04baa236470b543f7487184ddd4e6
eciara <ciara.ewelina@gmail.com>
2024-08-21 14:57:30 +0200

    Add image copy configuration to vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,31 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-
+const { defineConfig } = require('@vue/cli-service');
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src/assets/images'),
+            to: path.resolve(__dirname, 'dist/images'),
+            noErrorOnMissing: true,
+            globOptions: {
+              dot: true,
+              gitignore: true,
+              ignore: ['*.DS_Store']
+            }
+          }
+        ]
+      })
+    ]
+  },
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'scss',
-      // load which style file you want to import globally
       patterns: [path.resolve(__dirname, './src/assets/scss/style.scss')],
     },
   },
-};
+});
