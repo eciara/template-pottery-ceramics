d2b896067e43216be8d5b92420ca18009ac792c4
eciara <ciara.ewelina@gmail.com>
2024-08-21 15:00:34 +0200

    Add image copy configuration to vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = defineConfig({
         patterns: [
           {
             from: path.resolve(__dirname, 'src/assets/images'),
-            to: path.resolve(__dirname, 'dist/images'),
+            to: path.resolve(__dirname, 'dist/assets/images'),
             noErrorOnMissing: true,
             globOptions: {
               dot: true,
