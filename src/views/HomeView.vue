dfb52a18359b914af677d29e4685a5458c64d61a
eciara <ciara.ewelina@gmail.com>
2024-08-22 21:49:36 +0200

    Update file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@
         <OurServices />
 
         <GraphicBanner 
-            :backgroundImage="'./../images/banner/banner-2.jpg'" 
+            :backgroundImage="'./assets/images/banner/banner_2.jpg'" 
             subtitle="Pottery Workshop"
             title="Turning mud intro something amazing"
             copy="1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae sit, iste, iure tempora sequi doloremque, nemo voluptas neque magni harum itaque perferendis consectetur ad. Vero deserunt pariatur magni ex autem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae sit, iste, iure tempora sequi doloremque, nemo voluptas neque magni harum itaque perferendis consectetur ad. Vero deserunt pariatur magni ex autem."
@@ -19,12 +19,12 @@
             paragraph="Suspendisse interdum condimentum ipsum, nec rutrum libero pellentesque eu. Vestibulum consectetur commodo ex non semper. Phasellus egestas lacinia enim sit amet rhoncus. Aliquam non nunc augue. Aliquam bibendum neque et nunc bibendum placerat. Nunc urna metus, hendrerit quis viverra luctus, semper vitae mi. Duis tincidunt iaculis rhoncus." 
         />
 
-        <CounterComponent />
+        <Counter />
         <ChooseUs />
         <OurTeam />
 
         <GraphicBanner 
-            :backgroundImage="'./../images/5.jpg'" 
+            :backgroundImage="'./assets/images/5.jpg'" 
             customClass="flex-row-reverse"
             title="Turning mud intro somerhing amazing"
             copy="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
@@ -32,7 +32,7 @@
             btnText="Discover More" 
         />
 
-        <CustomersOpinion />
+        <Testimonials />
     </div>
 </template>
 
@@ -40,14 +40,14 @@
 
 import BannerTop from './../components/BannerTop';
 import ContactBlock from './../components/ContactBlock';
-import OurProducts from './../components/OurProducts';
+import OurProducts from './../components/OurProducts/OurProductsSection';
 import OurServices from './../components/ChooseUs/ChooseUsBlock';
 import GraphicBanner from './../components/GraphicBanner';
 import TextContainer from './../components/TextContainer';
-import CounterComponent from './../components/Counter/CounterComponent';
+import Counter from './../components/Counter/CounterComponent';
 import ChooseUs from './../components/ChooseUs/ChooseUsSection';
-import OurTeam from './../components/OurTeam';
-import CustomersOpinion from './../components/CustomersOpinion';
+import OurTeam from './../components/OurTeam/OurTeamSection';
+import Testimonials from './../components/TestimonialsSection';
 
 export default {
     name: 'HomeView',
@@ -58,10 +58,10 @@ export default {
         OurServices,
         GraphicBanner,
         TextContainer,
-        CounterComponent,
+        Counter,
         ChooseUs,
         OurTeam,
-        CustomersOpinion,
+        Testimonials,
     }
 }
 </script>
