709b4ff77c24b4e410fa3cc266fc73ea22f681d6
eciara <ciara.ewelina@gmail.com>
2024-08-22 21:52:37 +0200

    Adding change images

--- src/components/ChooseUs/ChooseUsFigure.vue
+++ src/components/ChooseUs/ChooseUsFigure.vue
@@ -2,10 +2,10 @@
     <div class="col-lg-6 choose-us__figure">
         <div class="row">
             <div class="col">
-                <ChooseUsImage src="./../../images/choose-us/choose-us_1.jpg" classModifier="--first" />
+                <ChooseUsImage src="./../assets/images/choose-us/choose-us_1.jpg" classModifier="--first" />
             </div>
             <div class="col">
-                <ChooseUsImage src="./../../images/choose-us/choose-us_2.jpg" classModifier="--second" />
+                <ChooseUsImage src="./../assets/images/choose-us/choose-us_2.jpg" classModifier="--second" />
             </div>
         </div>
     </div>
